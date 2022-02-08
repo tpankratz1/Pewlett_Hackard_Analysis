1dcbd7e8f316f7e5c55e8c7fb7232ef95db3e24b
tpankratz1 <96349572+tpankratz1@users.noreply.github.com>
2022-02-07 20:22:31 -0600

    added sql and csv files

--- schema.sql
+++ schema.sql
@@ -41,8 +41,7 @@ CREATE TABLE dept_emp (
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-  PRIMARY KEY (emp_no)
+  FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
 );
 
 CREATE TABLE titles (
@@ -50,8 +49,8 @@ CREATE TABLE titles (
   title VARCHAR NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
-  FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-  PRIMARY KEY (emp_no)
+  FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
 );
 
-SELECT * FROM departments;
+SELECT * FROM dept_emp;
+DROP TABLE dept_emp CASCADE;
